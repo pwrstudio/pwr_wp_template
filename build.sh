69b82c13c49c175b3b61cec13d74b0d552639d8d
Rasmus Svensson <rasmus@Rasmuss-Air.home>
2015-11-15 21:58:26 +0100

    Added build script. Added Handlebars.

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+TIME=`date +%Y-%m-%d-%H-%M`
+NAME=${PWD##*/}  
+mkdir -v ../$NAME-$TIME
+cp -v ./*.php ../$NAME-$TIME/
+cp -v ./main.min.js ../$NAME-$TIME/
+cp -v ./templates.js ../$NAME-$TIME/
+cp -v ./style.css ../$NAME-$TIME/
