456c3a9f815c3620d41589bc48f48658dd708099
PWR <cell@pwr.site>
2016-06-02 10:44:52 +0200

    removed lint. added standard js.

--- header.php
+++ header.php
@@ -7,25 +7,26 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
   <meta name="theme-color" content="#000">
 
-  <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
-  <link rel="manifest" href="favicon/manifest.json">
-  <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
+  <?php $tempdir = get_template_directory_uri();?>
   
-  <title><?php wp_title(" | ", TRUE, "RIGHT"); ?></title>
+  <link rel="apple-touch-icon" sizes="57x57" href="<?php echo $tempdir;?>/img/favicons/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="<?php echo $tempdir;?>/img/favicons/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="<?php echo $tempdir;?>/img/favicons/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $tempdir;?>/img/favicons/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="<?php echo $tempdir;?>/img/favicons/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $tempdir;?>/img/favicons/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="<?php echo $tempdir;?>/img/favicons/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $tempdir;?>/img/favicons/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $tempdir;?>/img/favicons/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="<?php echo $tempdir;?>/img/favicons/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $tempdir;?>/img/favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="<?php echo $tempdir;?>/img/favicons/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $tempdir;?>/img/favicons/favicon-16x16.png">
+  <link rel="manifest" href="<?php echo $tempdir;?>/img/favicons/manifest.json">
+  <meta name="msapplication-TileColor" content="#bebebe">
+  <meta name="msapplication-TileImage" content="<?php echo $tempdir;?>/img/favicons/ms-icon-144x144.png">
 
+  <title><?php wp_title(" | ", TRUE, "RIGHT"); ?></title>
 
   <?php wp_head(); ?>
     
