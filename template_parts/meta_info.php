8125ec29bd2b72f79806bb4459a61de36b9f2fc0
PWR <cell@pwr.site>
2016-09-13 18:43:18 +0200

    Added missing files :(

---
+++ template_parts/meta_info.php
@@ -0,0 +1,9 @@
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:site" content="@pwr_studio" />
+<meta name="twitter:creator" content="@pwr_studio" />
+
+<meta property="og:title" content="">
+<meta property="og:type" content="website">
+<meta property="og:url" content="">
+<meta property="og:image" content="">
+<meta property="og:description" content="" />
